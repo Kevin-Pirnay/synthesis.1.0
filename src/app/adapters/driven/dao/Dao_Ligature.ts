931e654ff63abb89e14193726adc52f542e6eea8
Kevin Pirnqy <kevinpirnqy@Kevins-MacBook-Air.local>
2023-08-10 16:52:03 +0200

    changing all relationship from ptr to ids

--- src/app/adapters/driven/dao/Dao_Ligature.ts
+++ src/app/adapters/driven/dao/Dao_Ligature.ts
@@ -1,6 +1,7 @@
+import { Container } from "../../../core/domain/entities/Container";
 import { Ligature } from "../../../core/domain/entities/Ligature";
 import { IDao_Ligature } from "../../../core/port/driven/dao/IDao_Ligature";
-import { Runtime_Persistence } from "../runtime_memory/Runtime_Persistence";
+import { ILigature_Data_Fix, ILigature_Data_Flow, Runtime_Persistence } from "../runtime_memory/Runtime_Persistence";
 
 export class Dao_Ligature implements IDao_Ligature
 {
@@ -8,23 +9,54 @@ export class Dao_Ligature implements IDao_Ligature
 
     public delete(ligature: Ligature): void 
     {
-        delete this.__runtime_persistence.ligatures[ligature.id];
+        const current_flow = this.__runtime_persistence.stack_flows.slice(-1)[0];
+
+        const index = this.__runtime_persistence.ligatures_ids.indexOf(ligature.id);
+        this.__runtime_persistence.ligatures_ids.splice(index, 1);
+        delete this.__runtime_persistence.ligatures_fix[ligature.id];
+        delete this.__runtime_persistence.ligatures_flow[ligature.id][current_flow];
     }
 
     public get_all(): Ligature[] 
     {
         const result : Ligature[] = [];
 
-        for(const [key, value] of Object.entries(this.__runtime_persistence.ligatures))
+        this.__runtime_persistence.ligatures_ids.forEach((id : string) =>
         {
-            result.push(value);
-        }
+            result.push(this.__assemble_ligature(id));
+        });
 
         return result;
     }
 
     public save(ligature: Ligature): void 
     {
-        this.__runtime_persistence.ligatures[ligature.id] = ligature;
+        const current_flow = this.__runtime_persistence.stack_flows.slice(-1)[0];
+
+        this.__runtime_persistence.ligatures_ids.push(ligature.id);
+        this.__runtime_persistence.ligatures_fix[ligature.id] = { id: ligature.id }
+        this.__runtime_persistence.ligatures_flow[ligature.id] = { };
+        this.__runtime_persistence.ligatures_flow[ligature.id][current_flow] = { parent : ligature.parent, child : ligature.child, positions : ligature.positions };
+    }
+
+    private __assemble_ligature(ligature_id : string) : Ligature
+    {
+        const current_flow = this.__runtime_persistence.stack_flows.slice(-1)[0];
+
+        const flow_data : ILigature_Data_Flow = this.__runtime_persistence.ligatures_flow[ligature_id][current_flow];
+
+        const fix_data : ILigature_Data_Fix = this.__runtime_persistence.ligatures_fix[ligature_id];
+
+        const id = fix_data.id;
+
+        const parent : Container = flow_data.parent;
+
+        const child : Container = flow_data.child;
+
+        const ligature = new Ligature(id, parent, child);
+
+        ligature.positions.abs_ratio.__.assign_new_data(flow_data.positions.abs_ratio);
+
+        return ligature;
     }
 }
--- src/app/adapters/driven/runtime_memory/Runtime_Persistence.ts
+++ src/app/adapters/driven/runtime_memory/Runtime_Persistence.ts
@@ -1,5 +1,5 @@
-import { Container_Positions, Node } from "../../../core/domain/entities/Container"
-import { Ligature } from "../../../core/domain/entities/Ligature"
+import { Container, Container_Positions, Node } from "../../../core/domain/entities/Container"
+import { Ligature, Ligature_Positions } from "../../../core/domain/entities/Ligature"
 
 export class Runtime_Persistence
 {
@@ -7,7 +7,10 @@ export class Runtime_Persistence
     public readonly containers_flow : { [id : string]: { [flow : string] : IContainer_Data_Flow } } = { }
     public readonly containers_fix : { [id : string]:  IContainer_Data_Fix } = { }
 
-    public readonly ligatures : { [id : string] : Ligature } = { }
+    public readonly ligatures_ids : string[] = [];
+    public readonly ligatures_flow : { [id : string]: { [flow : string] : ILigature_Data_Flow } } = { }
+    public readonly ligatures_fix : { [id : string]:  ILigature_Data_Fix } = { }
+
 
     public readonly stack_flows : string[] = [];
     public readonly flows : string[] = [];
@@ -23,4 +26,16 @@ export interface IContainer_Data_Flow
 {
     node : Node;
     positions : Container_Positions;
+}
+
+export interface ILigature_Data_Fix
+{
+    id : string;
+}
+
+export interface ILigature_Data_Flow
+{
+    parent : Container;
+    child : Container;
+    positions : Ligature_Positions;
 }
