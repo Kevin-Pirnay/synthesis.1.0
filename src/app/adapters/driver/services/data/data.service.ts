e1bdcfeeb20baaf6318fc8a4f6868e2d693f05ea
Kevin Pirnqy <kevinpirnqy@Kevins-MacBook-Air.local>
2023-08-21 14:14:35 +0200

    save refactoring data service

--- src/app/adapters/driver/services/data/data.service.ts
+++ src/app/adapters/driver/services/data/data.service.ts
@@ -8,116 +8,127 @@ import { Data_Type } from '../../../../core/domain/handlers/handlers_entities/Da
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class DataService 
 {
-  private __is_mouse_down_on_container : boolean = false;
-  private __is_mouse_down_on_grip : boolean = false;
-  private __is_mouse_down_on_ligature : boolean = false;
-  private __is_zooming : boolean = false;
-  private __is_view_moving : boolean = false;
+  private readonly __dtos : IDto[] = [];
 
+  private readonly __event : Event_State;
+  private readonly __focus : Focus_State;
+
+  private readonly __set : Set_State;
+  private readonly __ask : Ask_State;
+  private readonly __get : Get_Data_State;
+  private readonly __data : Data_Handler;
+
+  constructor()
+  {
+    this.__event = new Event_State();
+    this.__focus = new Focus_State();
+
+    this.__set = new Set_State(this.__event, this.__focus);
+    this.__ask = new Ask_State(this.__event, this.__focus);
+    this.__get = new Get_Data_State(this.__focus);
+    this.__data = new Data_Handler(this.__dtos);
+  }
+  
+  public set_mouse_is_down_on_a_container = (container : Container) : void => { this.__set.set_mouse_is_down_on_a_container(container); }
+  
+  public set_mouse_is_down_on_a_ligature = (ligature : Ligature) : void => { this.__set.set_mouse_is_down_on_a_ligature(ligature); }
+  
+  public set_mouse_is_down_on_a_grip = (ligature : Ligature) : void => { this.__set.set_mouse_is_down_on_a_grip(ligature); }
+  
+  public set_container_on_focus_from_dtos = (dtos : IDto[]) : void => { this.__set.set_container_on_focus_from_dtos(dtos); }
+
+  public set_mouse_is_up = () : void => { this.__set.set_mouse_is_up(); }
+
+  public set_container_on_focus = (container : Container | null) : void => { this.__set.set_container_on_focus(container); }
+
+  public set_ligature_on_focus = (ligature : Ligature | null) : void => { this.__set.set_ligature_on_focus(ligature); }
+
+  public set_is_zooming = (value : boolean) : void => { this.__set.set_is_zooming(value); }
+
+  public set_is_view_moving = (value : boolean) : void => { this.__set.set_is_view_moving(value); }
+
+  public is_mouse_down_on_something = () : boolean => { return this.__ask.is_mouse_down_on_something(); }
+
+  public is_mouse_down_on_container = () : boolean => { return this.__ask.is_mouse_down_on_container(); }
+
+  public is_mouse_down_on_grip = () : boolean => { return this.__ask.is_mouse_down_on_grip(); }
+
+  public is_there_a_container_on_focus = () : boolean => { return this.__ask.is_there_a_container_on_focus(); }
+
+  public is_zooming = () : boolean => { return this.__ask.is_zooming(); }
+
+  public is_view_moving = () : boolean => { return this.__ask.is_view_moving(); }
+
+  public container_on_focus = () : Container => { return this.__get.container_on_focus(); }
+
+  public ligature_on_focus = () : Ligature => { return this.__get.ligature_on_focus(); }
+
+  public add_dtos = (dtos : IDto[]) : void => { this.__data.add_dtos(dtos); }
+
+  public replace_its_current_dtos_by = (dtos : IDto[]) : void => { this.__data.replace_its_current_dtos_by(dtos); }
+
+  public replace_its_current_dtos_by_a_dto = (dto : IDto) : void =>  { this.__data.replace_its_current_dtos_by_a_dto(dto); }
+
+  public delete_from_its_dtos = (ids_to_delete: string[])  => { this.__data.delete_from_its_dtos(ids_to_delete); }
+
+}
+
+class Event_State
+{
+  public is_mouse_down_on_container : boolean = false;
+  public is_mouse_down_on_grip : boolean = false;
+  public is_mouse_down_on_ligature : boolean = false;
+  public is_zooming : boolean = false;
+  public is_view_moving : boolean = false;
+
+}
+
+class Focus_State
+{
   public container_currently_on_focus : Ptr<Container> = new Ptr();
-  private ligature_currently_on_focus : Ptr<Ligature> = new Ptr();
+  public ligature_currently_on_focus : Ptr<Ligature> = new Ptr();
+}
 
-  public readonly dtos : IDto[] = [];
+class Set_State
+{
+  constructor(private readonly __event : Event_State, private readonly __focus : Focus_State) { }
+  
+  private __reset_elements_on_focus()  : void
+  {
+    this.__focus.container_currently_on_focus._ = null;
+    this.__focus.ligature_currently_on_focus._ = null;
+  }
 
   public set_mouse_is_down_on_a_container(container : Container) : void
   {
     this.__reset_elements_on_focus();
-    this.__is_mouse_down_on_container = true;
-    this.container_currently_on_focus._ = container;
+    this.__event.is_mouse_down_on_container = true;
+    this.__focus.container_currently_on_focus._ = container;
   }
 
   public set_mouse_is_up() : void
   {
-    this.__is_mouse_down_on_container = false;
-    this.__is_mouse_down_on_grip = false;
-    this.__is_mouse_down_on_ligature = false;
-  }
-
-  private __reset_elements_on_focus()  : void
-  {
-    this.container_currently_on_focus._ = null;
-    this.ligature_currently_on_focus._ = null;
+    this.__event.is_mouse_down_on_container = false;
+    this.__event.is_mouse_down_on_grip = false;
+    this.__event.is_mouse_down_on_ligature = false;
   }
 
   public set_mouse_is_down_on_a_ligature(ligature : Ligature) : void
   {
     this.__reset_elements_on_focus();
-    this.__is_mouse_down_on_ligature = true;
-    this.ligature_currently_on_focus._ = ligature;
+    this.__event.is_mouse_down_on_ligature = true;
+    this.__focus.ligature_currently_on_focus._ = ligature;
   }
 
   public set_mouse_is_down_on_a_grip(ligature : Ligature) : void
   {
     this.__reset_elements_on_focus();
-    this.__is_mouse_down_on_grip = true;
-    this.ligature_currently_on_focus._ = ligature;
-  }
-
-  public is_mouse_down_on_something() : boolean
-  {
-    return this.__is_mouse_down_on_container 
-        || this.__is_mouse_down_on_ligature 
-        || this.__is_mouse_down_on_grip 
-        ? true : false;
-  }
-
-  public is_mouse_down_on_container() : boolean
-  {
-    return this.__is_mouse_down_on_container && this.container_currently_on_focus._ ? true : false;
-  }
-
-  public is_mouse_down_on_grip() : boolean
-  {
-    return this.__is_mouse_down_on_grip && this.ligature_currently_on_focus._ ? true : false;
-  }
-
-  public container_on_focus() : Container
-  {
-    if ( this.container_currently_on_focus._ == null ) throw new Error("No Container is currently on focus");
-
-    return this.container_currently_on_focus._;
-  }
-
-  public ligature_on_focus() : Ligature
-  {
-    if ( this.ligature_currently_on_focus._ == null ) throw new Error("No Ligature is currently on focus");
-
-    return this.ligature_currently_on_focus._;
-  }
-
-  public add_dtos(dtos : IDto[]) : void
-  {
-    dtos.forEach(dto => this.dtos.push(dto));
-  }
-
-  public replace_its_current_dtos_by(dtos : IDto[]) : void
-  {
-    this.dtos.length = 0; 
-        
-    dtos.forEach(dto => this.dtos.push(dto));
-  }
-
-  public replace_its_current_dtos_by_a_dto(dto : IDto) : void
-  {
-    this.dtos.length = 0; 
-        
-    this.dtos.push(dto);
-  }
-
-  public delete_from_its_dtos(ids_to_delete: string[]) 
-  {
-    const new_list = this.dtos.filter(dto => !ids_to_delete.includes(dto.element.id));
-
-    this.dtos.length = 0;
-
-    new_list.forEach(dto => this.dtos.push(dto));
-  }
-
-  public is_there_a_container_on_focus() : boolean
-  {
-    return this.container_currently_on_focus._ ? true : false;
+    this.__event.is_mouse_down_on_grip = true;
+    this.__focus.ligature_currently_on_focus._ = ligature;
   }
 
   public set_container_on_focus_from_dtos(dtos : IDto[]) : void
@@ -131,33 +142,114 @@ export class DataService
 
   public set_container_on_focus(container : Container | null) : void
   {
-    this.container_currently_on_focus._ = container;
+    this.__focus.container_currently_on_focus._ = container;
   }
 
   public set_ligature_on_focus(ligature : Ligature | null) : void
   {
-    this.ligature_currently_on_focus._ = ligature;
+    this.__focus.ligature_currently_on_focus._ = ligature;
   }
 
   public set_is_zooming(value : boolean) : void
   {
-    this.__is_zooming = value;
+    this.__event.is_zooming = value;
   }
 
   public set_is_view_moving(value : boolean) : void
   {
-    this.__is_view_moving = value;
+    this.__event.is_view_moving = value;
+  }
+}
+
+class Ask_State
+{
+  constructor(private readonly __event : Event_State, private readonly __focus : Focus_State) { }
+
+  public is_mouse_down_on_something() : boolean
+  {
+    return this.__event.is_mouse_down_on_container 
+        || this.__event.is_mouse_down_on_ligature 
+        || this.__event.is_mouse_down_on_grip 
+        ? true : false;
+  }
+
+  public is_mouse_down_on_container() : boolean
+  {
+    return this.__event.is_mouse_down_on_container && this.__focus.container_currently_on_focus._ ? true : false;
+  }
+
+  public is_mouse_down_on_grip() : boolean
+  {
+    return this.__event.is_mouse_down_on_grip && this.__focus.ligature_currently_on_focus._ ? true : false;
+  }
+
+  public is_there_a_container_on_focus() : boolean
+  {
+    return this.__focus.container_currently_on_focus._ ? true : false;
   }
 
   public is_zooming() : boolean
   {
-    return this.__is_zooming;
+    return this.__event.is_zooming;
   }
 
   public is_view_moving() : boolean
   {
-    return this.__is_view_moving;
+    return this.__event.is_view_moving;
   }
 }
+
+class Get_Data_State
+{
+  constructor(private readonly __focus : Focus_State) { }
+
+  public container_on_focus() : Container
+  {
+    if ( this.__focus.container_currently_on_focus._ == null ) throw new Error("No Container is currently on focus");
+
+    return this.__focus.container_currently_on_focus._;
+  }
+
+  public ligature_on_focus() : Ligature
+  {
+    if ( this.__focus.ligature_currently_on_focus._ == null ) throw new Error("No Ligature is currently on focus");
+
+    return this.__focus.ligature_currently_on_focus._;
+  }
+}
+
+class Data_Handler
+{
+  constructor(private readonly __dtos : IDto[]) { }
+
+  public add_dtos(dtos : IDto[]) : void
+  {
+    dtos.forEach(dto => this.__dtos.push(dto));
+  }
+
+  public replace_its_current_dtos_by(dtos : IDto[]) : void
+  {
+    this.__dtos.length = 0; 
+        
+    dtos.forEach(dto => this.__dtos.push(dto));
+  }
+
+  public replace_its_current_dtos_by_a_dto(dto : IDto) : void
+  {
+    this.__dtos.length = 0; 
+        
+    this.__dtos.push(dto);
+  }
+
+  public delete_from_its_dtos(ids_to_delete: string[]) 
+  {
+    const new_list = this.__dtos.filter(dto => !ids_to_delete.includes(dto.element.id));
+
+    this.__dtos.length = 0;
+
+    new_list.forEach(dto => this.__dtos.push(dto));
+  }
+}
+
 
 
