240f675d2699aef9d01c0b3e55ed7f22b87a9435
Kevin Pirnqy <kevinpirnqy@kevins-air.home>
2023-08-24 11:14:32 +0200

    save refactor app

---
+++ src/app/adapters/driver/app.module.ts
@@ -0,0 +1,32 @@
+import { LinkRootsComponent } from './components/aside/link-roots/link-roots.component';
+import { PaginateComponent } from './components/aside/paginate/paginate.component';
+import { ChooseRootComponent } from './components/aside/choose-root/choose-root.component';
+import { BackViewComponent } from './components/aside/back-view/back-view.component';
+import { AsideComponent } from './components/aside/base/aside.component';
+import { MenuComponent } from './components/aside/menu/menu.component';
+import { SvgComponent } from './components/svg/svg.component';
+import { AppComponent } from './base/app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SvgComponent,
+    MenuComponent,
+    AsideComponent,
+    BackViewComponent,
+    ChooseRootComponent,
+    PaginateComponent,
+    LinkRootsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { AppModule } from './app/adapters/driver/app.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
